fa0ae38416153718bd4386fba36d5df96b1a1714
echocortuna <echocortuna@users.noreply.github.com>
2020-06-20 21:57:14 +0800

    heroku

--- django_site/settings.py
+++ django_site/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
